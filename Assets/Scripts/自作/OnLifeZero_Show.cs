37766c6d6a4d9544d2a9478b09bcc2a4e0f0c049
UEJIN <sixthmirage1224@gmail.com>
2021-07-27 02:17:40 +0900

    オンラインスコア実装031

--- Assets/Scripts/自作/OnLifeZero_Show.cs
+++ Assets/Scripts/自作/OnLifeZero_Show.cs
@@ -8,6 +8,7 @@ public class OnLifeZero_Show : MonoBehaviour {
     public string showObjectName;   // 表示オブジェクト名：Inspectorで指定
     public string groundTag = "ground";  // 地面判定タグ：Inspectorで指定
 	public string ceilingTag = "ceiling";       // 天井判定タグ：Inspectorで指定
+	bool isFinish;
     GameObject showObject;
 
     void Start() { // 最初に行う
@@ -18,12 +19,16 @@ public class OnLifeZero_Show : MonoBehaviour {
 
 	void Update()
 	{
-		if (LifeManager.LifeNum == 0)
+		if (!isFinish && LifeManager.LifeNum == 0)
 		{
-			showObject.SetActive(true); // 消していたものを表示する
+			isFinish = true;
+			Invoke("ShowwObject", 2.0f);//二秒後に呼び出す
 
 		}
 
 	}
-	
+	void ShowwObject()
+    {
+		showObject.SetActive(true); // 消していたものを表示する
+	}
 }
