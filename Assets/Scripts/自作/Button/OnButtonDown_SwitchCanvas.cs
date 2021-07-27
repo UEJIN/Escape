9b91bc7bdc6382643d0fc34e94f950dc6873fd12
UEJIN <sixthmirage1224@gmail.com>
2021-07-28 01:44:40 +0900

    タイトル画面調整

--- Assets/Scripts/自作/Button/OnButtonDown_SwitchCanvas.cs
+++ Assets/Scripts/自作/Button/OnButtonDown_SwitchCanvas.cs
@@ -2,26 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// タッチすると、シーンを切り換える
+// タッチすると、キャンバスを切り換える
 public class OnButtonDown_SwitchCanvas : MonoBehaviour {
 
 	public GameObject NowCanvas;  // シーン名：Inspectorで指定
     public GameObject NextCanvas;  // シーン名：Inspectorで指定
-
+    public float DelayTime=5f;
     public ButtonState SceneMoveButton;
 
     void FixedUpdate() 
 	{ // タッチしたら
       // シーンを切り換える
+        Invoke("SwitchCanvas", DelayTime);
 
+    }
+	void Update()
+    {
+
+	}
+    void SwitchCanvas()
+    {
         if (SceneMoveButton.IsDown() == true)
         {
             NowCanvas.SetActive(false);
             NextCanvas.SetActive(true);
         }
     }
-	void Update()
-    {
-
-	}
 }
