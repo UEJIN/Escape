03c76be4530b86e794bf6b15c34ba5890d6f7401
UEJIN <sixthmirage1224@gmail.com>
2021-07-28 20:37:47 +0900

    Admob実装

---
+++ Assets/Scripts/GoogleAds.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class GoogleAds : MonoBehaviour
+{
+    public string appId = "ca-app-pub-3940256099942544~3347511713";
+    public string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+
+    // Use this for initialization
+    void Start()
+    {
+        //// アプリID、 これはテスト用
+        //string appId = "ca-app-pub-3940256099942544~3347511713";
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(initStatus => { });
+
+        RequestBanner();
+    }
+    private void RequestBanner()
+    {
+
+        //// 広告ユニットID これはテスト用
+        //string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+
+        // Create a 320x50 banner at the top of the screen.
+        BannerView bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the banner with the request.
+        bannerView.LoadAd(request);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
