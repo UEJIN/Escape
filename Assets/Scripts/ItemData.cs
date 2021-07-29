bf2433ce8c0cbbbdf6b38c274968afa358f81ed9
UEJIN <sixthmirage1224@gmail.com>
2021-07-30 00:23:07 +0900

    インベントリ製作中

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,61 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using NaughtyAttributes;
+
+[Serializable]
+[CreateAssetMenu(fileName = "Item", menuName = "MyScriptable/CreateItem")]
+public class ItemData : ScriptableObject
+{
+
+	public enum KindOfItem
+	{
+		EquipableItem,
+		UseItem
+	}
+
+	//　アイテムの種類
+	[SerializeField]
+	private KindOfItem kindOfItem;
+	//　アイテムのアイコン
+	[SerializeField]
+    [ShowAssetPreview]
+    private Sprite icon;
+	//　アイテムの名前
+	[SerializeField]
+	private string itemName;
+	//　アイテムの情報
+	[SerializeField]
+	private string information;
+	//　アイテムの所持
+	[SerializeField]
+	private bool isPossetion;
+	//　アイテムの装備
+	[SerializeField]
+	private bool isEquip;
+	//　アイテムの効果
+	//[SerializeField]
+	//public GameObject Effects;
+
+	public KindOfItem GetKindOfItem()
+	{
+		return kindOfItem;
+	}
+
+	public Sprite GetIcon()
+	{
+		return icon;
+	}
+
+	public string GetItemName()
+	{
+		return itemName;
+	}
+
+	public string GetInformation()
+	{
+		return information;
+	}
+}
