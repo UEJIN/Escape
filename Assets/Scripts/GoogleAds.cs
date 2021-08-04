ebca308f2718257d3edee83a2a29d42a12fa608c
UEJIN <sixthmirage1224@gmail.com>
2021-08-05 00:41:28 +0900

    広告を任意の画面でのみオフにする

--- Assets/Scripts/GoogleAds.cs
+++ Assets/Scripts/GoogleAds.cs
@@ -2,13 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using GoogleMobileAds.Api;
+using UnityEngine.SceneManagement;
 
 public class GoogleAds : MonoBehaviour
 {
     public string appId = "ca-app-pub-3940256099942544~3347511713";
     public string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+    //public string[] AdOffSceneNames;
+    public GameObject[] AdOffObjects;
+    private bool OffFlag = false;
+    BannerView bannerView;
+    AdRequest request;
+
 
     // Use this for initialization
+
+    void Awale()
+    {
+
+
+    }
+
     void Start()
     {
         //// アプリID、 これはテスト用
@@ -26,10 +40,10 @@ public class GoogleAds : MonoBehaviour
         //string adUnitId = "ca-app-pub-3940256099942544/6300978111";
 
         // Create a 320x50 banner at the top of the screen.
-        BannerView bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+        bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
 
         // Create an empty ad request.
-        AdRequest request = new AdRequest.Builder().Build();
+        request = new AdRequest.Builder().Build();
 
         // Load the banner with the request.
         bannerView.LoadAd(request);
@@ -40,6 +54,53 @@ public class GoogleAds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //特定の画面で非表示
+
+
+    }
+
+    void FixedUpdate()
+    {
+        //特定のキャンバスが有効ならオフにする
+        for (int i = 0; i < AdOffObjects.Length; i++)
+        {
+            if (AdOffObjects[i].activeSelf)
+            {
+                OffFlag = true;
+                break;
+            }
+            else 
+            {
+                OffFlag = false;
+            }
+
+        }
+
+        if (OffFlag == true && bannerView != null)
+        {
+            bannerView.Destroy();
+            bannerView = null;
+        }
+
+        if (OffFlag == false && bannerView == null) //オフシーンでなくて消えていたら再生成
+        {
+            RequestBanner();
+        }
+
+        //for (int i = 0; i < AdOffSceneNames.Length; i++)
+        //{
+        //    //シーン名を判別。オフシーンならオフにする
+        //    if (SceneManager.GetActiveScene().name == AdOffSceneNames[i])
+        //    {
+        //        bannerView.Destroy();
+        //    }
+        //    else if (bannerView == null)　//オフシーンでなくて消えていたら再生成
+        //    {
+        //        RequestBanner();
+        //    }
+
+        //}
+
 
     }
 }
---
+++ Assets/Scripts/自作/Gacha/GachaManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GachaManager : MonoBehaviour
+{
+    //各レアリティの排出率を手動で設定
+    public float SSRratio = 2f;
+    public float SRratio = 18f;
+    public float Rratio= 30f;
+    public float Nratio = 50f;
+
+    //各レアリティのアイテムを手動で追加
+    public GameObject[] SSRitems;
+    public GameObject[] SRitems;
+    public GameObject[] Ritems;
+    public GameObject[] Nitems;
+
+    //ガチャボタンを指定
+    public ButtonState GachaButtonState;
+
+    //最終結果アイテム
+    private GameObject ResultItem;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ガチャボタンが押されたら
+        if(GachaButtonState.IsDown())
+        {
+            //ポイントを消費
+
+            //アイテム決定
+            ResultItem = ChoseGachaResult();
+            //アイテムを取得状態にする
+            if (ResultItem.GetComponent<ItemData>().isPossession == false)
+            {
+                ResultItem.GetComponent<ItemData>().isPossession = true;
+            }
+            //ガチャ演出
+
+        }
+    }
+    
+    //ガチャの内容物を初期化
+    //private void InitGachaData()
+    //{
+    //    _gachaDataList = new[]
+    //    {
+    //        new GachaData("ョ", "Normal", Color.blue),
+    //        new GachaData("ねこ", "Normal", Color.blue),
+    //        ... (略) ...
+    //    };
+    //}
+
+    private GameObject ChoseGachaResult()
+    {
+        //レアリティ抽選して該当のリストを取得
+        GameObject[] GachaList = ChoseRarity();
+
+        //リストからランダムでアイテムを抽選して返す
+        return GachaList[Random.Range(0, GachaList.Length)];
+    }
+
+    private GameObject[] ChoseRarity()
+    {
+        var randValue = Random.Range(0, 100);
+        if (randValue <= SSRratio)
+        {
+            return SSRitems;
+        }
+        if (randValue <= SSRratio + SRratio)
+        {
+            return SRitems;
+        }
+        if (randValue <= SSRratio + SRratio + Rratio)
+        {
+            return Ritems;
+        }
+        return Nitems;
+    }
+
+
+}
