0662fc67828c0b553ae4c792a4a4446471a40e59
UEJIN <sixthmirage1224@gmail.com>
2021-09-03 00:38:32 +0900

    ポーズ機能追加

---
+++ Assets/Scripts/自作/Button/OnButtonDown_Pause.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+// タッチすると、キャンバスを切り換える
+public class OnButtonDown_Pause : MonoBehaviour
+{
+    //public float DelayTime = 1f;
+    ButtonState buttonState;
+
+
+    void OnEnable()
+    {
+        buttonState = this.gameObject.GetComponent<ButtonState>();
+    }
+
+    void Update()
+    { // タッチしたら
+      // シーンを切り換える
+        //Invoke("SwitchCanvas", DelayTime);
+        SwitchCanvas();
+    }
+
+    void SwitchCanvas()
+    {
+        if (buttonState.IsDown() == true)
+        {
+            if (Time.timeScale != 0f)
+            {
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+
+        }
+    }
+}
